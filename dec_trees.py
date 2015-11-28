408bed3e58467e723a343496924e0744aedee1c3
Nick Aldershof <Naldershof@gmail.com>
2015-11-27 23:29:14 -0600

    Writing custom decision trees, starting entropy calculations and outlining

--- dec_trees.py
+++ dec_trees.py
@@ -1,64 +1,44 @@
 #!/usr/bin/env python
 
 import data_prep
-import mpaa_analysis
 import pandas as pd
 import numpy as np
-
-from collections import Counter
-import math
 
 #Plan: Create a custom decision tree algorithm here as well as a random
 # forest implementation and compare those to the sklearn implementation
 
 #-----------------------------
-#Example implementation from
-#data science from scratch
+#ID3 Algorithm
 #-----------------------------
 
-#Why log base 2? Fuck if I know. Magic. This is how we calculate entropy
-# though. Blame Shannon
+# Summary (From Wikipedia)
+#   Calculate the entropy of every attribute using the data set S
+#   Split the set S into subsets using the attribute for which entropy is minimum (or, equivalently, information gain is maximum)
+#   Make a decision tree node containing that attribute
+#   Recurse on subsets using remaining attributes.
 
 
-def entropy(class_probabilities):
-    """given a list of class probabilities, compute the entropy"""
-    return sum(-p * math.log(p, 2)
-               for p in class_probabilities
-               if p)  # ignore zero probabilities
+def entropy(dataset, column):
+    counts = dataset[column].value_counts()
+    total = counts.sum()
+    prob = counts / total
+    return sum(-p * np.log2(p) for p in prob)
 
 
-def class_probabilities(labels):
-    total_count = len(labels)
-    return [count / total_count
-            for count in Counter(labels).values()]
-
-
-def data_entropy(labeled_data):
-    labels = [label for _, label in labeled_data]
-    probabilities = class_probabilities(labels)
-    return entropy(probabilities)
-
-
-def partition_entropy(subsets):
-    """find the entropy from this partition of data into subsets
-    subsets is a list of lists of labeled data"""
-    total_count = sum(len(subset) for subset in subsets)
-    return sum(data_entropy(subset) * len(subset) / total_count
-               for subset in subsets)
-
-#-----------------------------
-#End Examples
-#-----------------------------
-
-
-def fit_trees():
+def fit_trees(train, label):
     pass
 
 
-def pred_trees():
+def pred_trees(test):
     pass
 
 if __name__ == '__main__':
     #Use the trees from mpaa_analysis and the fit and pred from the custom implementation
     #and compare the results
+
+    genre_listing = data_prep.read_genres('imdb/genres.list')
+    rating_listing = data_prep.read_ratings('imdb/ratings.list')
+    mpaa = data_prep.read_mpaa('imdb/mpaa-ratings-reasons.list')
+
+    movies = data_prep.merge_all(rating_listing, genre_listing, mpaa)
     pass
